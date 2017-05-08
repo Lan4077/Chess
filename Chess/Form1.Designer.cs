218f88c8838b70e4b89e0f416cf25b8b5fab994a
Lan4077 <Lan4077@yandex.ru>
2017-05-08 14:27:18 +0300

    moore logica

--- Chess/Form1.Designer.cs
+++ Chess/Form1.Designer.cs
@@ -32,13 +32,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.START_BUTTON = new System.Windows.Forms.Button();
             this.EXIT_BUTTON = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.Random_button = new System.Windows.Forms.RadioButton();
-            this.White_button = new System.Windows.Forms.RadioButton();
-            this.black_button = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
+            this.stopgame_button = new System.Windows.Forms.Button();
+            this.xod_partii = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -48,13 +45,13 @@
             this.pictureBox1.Size = new System.Drawing.Size(640, 640);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // START_BUTTON
             // 
             this.START_BUTTON.Location = new System.Drawing.Point(658, 629);
             this.START_BUTTON.Name = "START_BUTTON";
-            this.START_BUTTON.Size = new System.Drawing.Size(134, 23);
+            this.START_BUTTON.Size = new System.Drawing.Size(86, 23);
             this.START_BUTTON.TabIndex = 1;
             this.START_BUTTON.Text = "Начать игру";
             this.START_BUTTON.UseVisualStyleBackColor = true;
@@ -62,75 +59,50 @@
             // 
             // EXIT_BUTTON
             // 
-            this.EXIT_BUTTON.Location = new System.Drawing.Point(798, 629);
+            this.EXIT_BUTTON.Location = new System.Drawing.Point(852, 629);
             this.EXIT_BUTTON.Name = "EXIT_BUTTON";
-            this.EXIT_BUTTON.Size = new System.Drawing.Size(129, 23);
+            this.EXIT_BUTTON.Size = new System.Drawing.Size(75, 23);
             this.EXIT_BUTTON.TabIndex = 2;
             this.EXIT_BUTTON.Text = "АЛТ+Ф4";
             this.EXIT_BUTTON.UseVisualStyleBackColor = true;
             this.EXIT_BUTTON.Click += new System.EventHandler(this.EXIT_BUTTON_Click);
             // 
-            // groupBox1
+            // stopgame_button
             // 
-            this.groupBox1.Controls.Add(this.Random_button);
-            this.groupBox1.Controls.Add(this.White_button);
-            this.groupBox1.Controls.Add(this.black_button);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(658, 458);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(269, 165);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Настройки игры";
+            this.stopgame_button.Location = new System.Drawing.Point(750, 629);
+            this.stopgame_button.Name = "stopgame_button";
+            this.stopgame_button.Size = new System.Drawing.Size(96, 23);
+            this.stopgame_button.TabIndex = 4;
+            this.stopgame_button.Text = "Дезертировать";
+            this.stopgame_button.UseVisualStyleBackColor = true;
+            this.stopgame_button.Click += new System.EventHandler(this.stopgame_button_Click);
             // 
-            // Random_button
+            // xod_partii
             // 
-            this.Random_button.AutoSize = true;
-            this.Random_button.Location = new System.Drawing.Point(6, 78);
-            this.Random_button.Name = "Random_button";
-            this.Random_button.Size = new System.Drawing.Size(64, 17);
-            this.Random_button.TabIndex = 3;
-            this.Random_button.TabStop = true;
-            this.Random_button.Text = "Рандом";
-            this.Random_button.UseVisualStyleBackColor = true;
+            this.xod_partii.Location = new System.Drawing.Point(664, 28);
+            this.xod_partii.Name = "xod_partii";
+            this.xod_partii.ReadOnly = true;
+            this.xod_partii.Size = new System.Drawing.Size(263, 131);
+            this.xod_partii.TabIndex = 5;
+            this.xod_partii.Text = "";
             // 
-            // White_button
+            // label2
             // 
-            this.White_button.AutoSize = true;
-            this.White_button.Location = new System.Drawing.Point(6, 55);
-            this.White_button.Name = "White_button";
-            this.White_button.Size = new System.Drawing.Size(58, 17);
-            this.White_button.TabIndex = 2;
-            this.White_button.TabStop = true;
-            this.White_button.Text = "Белые";
-            this.White_button.UseVisualStyleBackColor = true;
-            // 
-            // black_button
-            // 
-            this.black_button.AutoSize = true;
-            this.black_button.Location = new System.Drawing.Point(6, 32);
-            this.black_button.Name = "black_button";
-            this.black_button.Size = new System.Drawing.Size(65, 17);
-            this.black_button.TabIndex = 1;
-            this.black_button.TabStop = true;
-            this.black_button.Text = "Чёрные";
-            this.black_button.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Сторона:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(664, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ход партии";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(939, 659);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.xod_partii);
+            this.Controls.Add(this.stopgame_button);
             this.Controls.Add(this.EXIT_BUTTON);
             this.Controls.Add(this.START_BUTTON);
             this.Controls.Add(this.pictureBox1);
@@ -138,9 +110,8 @@
             this.Name = "Form1";
             this.Text = "ШОХМАТЫ";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -148,12 +119,10 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button EXIT_BUTTON;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton Random_button;
-        private System.Windows.Forms.RadioButton White_button;
-        private System.Windows.Forms.RadioButton black_button;
         public System.Windows.Forms.Button START_BUTTON;
+        private System.Windows.Forms.Button stopgame_button;
+        private System.Windows.Forms.RichTextBox xod_partii;
+        private System.Windows.Forms.Label label2;
     }
 }
 
---
+++ Chess/picturebox_click_check.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class picturebox_click_check
+    {
+        public int[] rachet(int[,,,] array,int x = 0, int y = 0)
+        {
+            int[] figura = new int[4];
+            if(x - 248 < 0) //Ща будыт жара из сложных ифоф
+            {
+                if (x - 104 < 0)
+                {
+                    figura[0] = 0;
+                }
+                else if(x - 104 == 0)
+                {
+                    figura[0] = 1;
+                }
+                else
+                {
+                    if(x - 176 >= 0)
+                    {
+                        figura[0] = 2;
+                    }
+                    else
+                    {
+                        figura[0] = 1;
+                    }
+                }
+            }
+            else if(x-248 == 0)
+            {
+                figura[0] = 3;
+            }
+            else
+            {
+                if(x - 393 < 0)
+                {
+                    if(x - 321 < 0)
+                    {
+                        figura[0] = 3;
+                    }
+                    else
+                    {
+                        figura[0] = 4;
+                    }
+                }
+                else if(x - 393 == 0)
+                {
+                    figura[0] = 5;
+                }
+                else
+                {
+                    if(x - 465 < 0)
+                    {
+                        figura[0] = 5;
+                    }
+                    else if(x - 465 == 0)
+                    {
+                        figura[0] = 6;
+                    }
+                    else
+                    {
+                        if(x-537 >= 0)
+                        {
+                            figura[0] = 7;
+                        }
+                        else
+                        {
+                            figura[0] = 6;
+                        }
+                    }
+                }
+            }
+            return figura;
+        }
+    }
+}
