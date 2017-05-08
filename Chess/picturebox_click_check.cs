22a1e5b4a94f8a5d0d16cce5196fa8a835fade45
Lan4077 <Lan4077@yandex.ru>
2017-05-08 14:29:45 +0300

    lol

--- Chess/picturebox_click_check.cs
+++ Chess/picturebox_click_check.cs
@@ -9,71 +9,78 @@ namespace Chess
 {
     class picturebox_click_check
     {
-        public int[] rachet(int[,,,] array,int x = 0, int y = 0)
+        public int[] rachet(int[,,,] array, int x = 0, int y = 0)
         {
             int[] figura = new int[4];
-            if(x - 248 < 0) //Ща будыт жара из сложных ифоф
+            if (x - 32 < 0 || x - 607 > 0 || y - 32 < 0 || y - 607 < 0) //Ща будыт жара из сложных ифоф
             {
-                if (x - 104 < 0)
-                {
-                    figura[0] = 0;
-                }
-                else if(x - 104 == 0)
-                {
-                    figura[0] = 1;
-                }
-                else
-                {
-                    if(x - 176 >= 0)
-                    {
-                        figura[0] = 2;
-                    }
-                    else
-                    {
-                        figura[0] = 1;
-                    }
-                }
-            }
-            else if(x-248 == 0)
-            {
-                figura[0] = 3;
+                //NOTHING AZAZAZA
             }
             else
             {
-                if(x - 393 < 0)
+                if (x - 248 < 0)
                 {
-                    if(x - 321 < 0)
+                    if (x - 104 < 0)
                     {
-                        figura[0] = 3;
+                        figura[0] = 0;
+                    }
+                    else if (x - 104 == 0)
+                    {
+                        figura[0] = 1;
                     }
                     else
                     {
-                        figura[0] = 4;
-                    }
-                }
-                else if(x - 393 == 0)
-                {
-                    figura[0] = 5;
-                }
-                else
-                {
-                    if(x - 465 < 0)
-                    {
-                        figura[0] = 5;
-                    }
-                    else if(x - 465 == 0)
-                    {
-                        figura[0] = 6;
-                    }
-                    else
-                    {
-                        if(x-537 >= 0)
+                        if (x - 176 >= 0)
                         {
-                            figura[0] = 7;
+                            figura[0] = 2;
                         }
                         else
                         {
+                            figura[0] = 1;
+                        }
+                    }
+                }
+                else if (x - 248 == 0)
+                {
+                    figura[0] = 3;
+                }
+                else
+                {
+                    if (x - 393 < 0)
+                    {
+                        if (x - 321 < 0)
+                        {
+                            figura[0] = 3;
+                        }
+                        else
+                        {
+                            figura[0] = 4;
+                        }
+                    }
+                    else if (x - 393 == 0)
+                    {
+                        figura[0] = 5;
+                    }
+                    else
+                    {
+                        if (x - 465 < 0)
+                        {
+                            figura[0] = 5;
+                        }
+                        else if (x - 465 == 0)
+                        {
                             figura[0] = 6;
+                        }
+                        else
+                        {
+                            if (x - 537 >= 0)
+                            {
+                                figura[0] = 7;
+                            }
+                            else
+                            {
+                                figura[0] = 6;
+                            }
                         }
                     }
                 }
